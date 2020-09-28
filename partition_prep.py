3d71a509521a155dca690518410197746d2279bd
rumram <50412957+rumram@users.noreply.github.com>
2020-09-28 09:12:03 +0200

    Create partition_prep.py

---
+++ partition_prep.py
@@ -0,0 +1,19 @@
+# Get partitionfinder2 partitions file from Geneious annotation file
+
+def get_partitions(outfile, infile):
+	with open(outfile, 'w') as nf:
+		lines = open(infile, 'r').read().splitlines()
+		counter = 0
+		for line in lines:
+			counter += 1
+			lname = line.split("\t")[0]
+			pos1 = line.split("\t")[1]
+			pos2 = line.split("\t")[2]
+			if lname == 'noncoding':
+				nf.write("sub_" + str(counter) + " = " + pos1 + "-" + pos2 + ";" + "\n")
+			if lname == 'coding':
+				for i in range(0, 3):
+					nf.write("sub_" + str(counter) + "_pos" + str(i+1) + " = " + str(int(pos1) + i) + "-" + pos2 + "\\3;" + "\n")
+
+# Run function with specified input, output file names
+get_partitions('pf.cfg', 'partition.txt')
